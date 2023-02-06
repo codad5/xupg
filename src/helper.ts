8bb8158ae84dd05aaebec7debc60a9f32dd8d0b8
cadad5 <aniezeoformic@gmail.com>
2023-02-06 04:27:27 +0000

    added download progress bar

--- src/helper.ts
+++ src/helper.ts
@@ -1,11 +1,15 @@
 import AdmZip from 'adm-zip'
 import { doesNotMatch } from 'assert';
 import { ChildProcess,spawn, spawnSync } from 'child_process';
-import { existsSync } from 'fs';
+import { OptionValues } from 'commander';
+import { existsSync, createReadStream, createWriteStream } from 'fs';
 import { version } from 'os';
 import path from 'path';
 import { exit } from 'process';
+import axios from 'axios';
 import { _Modules } from './types';
+import ProgressBar from 'progress';
+import download from 'download';
 
 // const modules = ['full', 'php', 'phpmyadmin']
 export function getVersion() : string
@@ -18,13 +22,26 @@ export function getHomeDir() : string
     return require('os').homedir()
 }
 
-export function getNewZipDetail()
+export async function getNewZipDetail()
 {
     //  code to get new zip details
-    return {
-        href:'../test.zip',
-        version:1.0
+    try{
+        const res = await axios.get("https://sourceforge.net/projects/xampp/best_release.json")
+        if (res.data?.release){
+            return {
+                href : res.data.release.url as string,
+                version: res.data.release.date,
+                file_name: res.data.release.filename as string
+            }
+        }
+        return false
     }
+    catch(e)
+    {
+        console.log('Error', e)
+        exit(2)
+    }
+    
 }
 
 export function isAnUpdate(version:number)
@@ -33,23 +50,58 @@ export function isAnUpdate(version:number)
     return true;
 }
 
-export function downloadUpdate() : String
+export async function downloadUpdate() : Promise<String>
 {
     //get update detail
-    const update = getNewZipDetail()
+    const update = await getNewZipDetail()
     //check if it is an update 
+    console.log(update)
+    if(!update) throw new Error('Something went wrong')
     if(!isAnUpdate(update.version)) throw new Error("Version up to Date")
     // code to download from sourceforge
     // download(update.src, '../')
-    return "./test.zip";
+    let {href, file_name} = update
+    if(href.includes('.7z')) href = href.replace('.7z', '.zip')
+    console.log("downloading ", href)
+    // const d =  await download(update.href, './download')
+    const d = await downloadFile(update.href as string, update.file_name)
+    // console.log('done downloading')
+    return file_name;
+}
+function downloadFile(url : string, file_name:string)
+{
+    file_name = file_name.replace('.7z', '.zip')
+    file_name = path.basename(file_name)
+    console.log(path.resolve('./download/'+file_name))
+    const writeStream = createWriteStream(`./download/${file_name}`)
+    const readStream = download(url)
+    readStream.on('response', function (res) {
+        const len = parseInt(res.headers['content-length'] ?? '', 10)
+        const bar = new ProgressBar('  downloading [:bar] :rate/bps :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: len
+        })
+        readStream.on('data', function (chunk) {
+            writeStream.write(chunk)
+            bar.tick(chunk.length)
+        })
+        readStream.on('end', function () {
+            console.log('Download done with success\n')
+            writeStream.end()
+        })
+    })
+    return readStream
 }
 
 export function extractAndMove(newZipSrc: string, option: { module : _Modules, install_dir: string } = { install_dir: `${getHomeDir()}/xampp/`, module : 'php'})
 {   
+    console.log(path.resolve(newZipSrc))
     const zip = new AdmZip(newZipSrc)
     switch (option.module){
         case 'php':
-            finalExtract(zip, "test/2/php/", `${option.install_dir}/php`);
+            finalExtract(zip, "test_zip/2/php/", `${option.install_dir}/php`);
         break;
     }
 }
@@ -57,6 +109,7 @@ export function extractAndMove(newZipSrc: string, option: { module : _Modules, i
 function finalExtract(zip: AdmZip, entry:string, to:string)
 {
     if(!entry.endsWith('/')) entry+="/"
+    console.log('here', entry)
     zip.getEntries().forEach(element => {
         // console.log(element.toString())
         if (element.entryName.startsWith(entry)) {
@@ -72,19 +125,30 @@ function finalExtract(zip: AdmZip, entry:string, to:string)
     return true;
 }
 
-export function updatePHP(path = `${getHomeDir()}/xampp/`)
+export async function updatePHP(path = `${getHomeDir()}/xampp/`)
 {
     try{
         const p = require('path')
         console.log(p.resolve(path))
         if(!existsSync(path)) throw new Error(`xampp - php not found in ${path}`);
-        const download = downloadUpdate()
-        const done = extractAndMove(download as string, {module:'php', install_dir:'../test/2/php'})
-        return done;
+        const d = await downloadUpdate();
+        const done = extractAndMove(d as string, { module: 'php', install_dir: path });
+        return true;
+        // .catch(e)
+        // return true;
     }catch(e)
     {
         console.log(e)
-        exit(500)
+        exit(2)
     }
 
 }
+
+// export function update(mod: _Modules, options: OptionValues)
+// {
+//     switch(mod)
+//     {
+//         case 'php' :
+//             updatePHP(options.dir ??)
+//     }
+// }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import { Command } from "commander"
 import { getHomeDir, getVersion, updatePHP } from "./helper";
 import fs from 'fs'
+import { exit } from "process";
 
 const program = new Command;
 const DEFAULT_DIR = `${getHomeDir()}/xampp/`;
@@ -16,16 +17,33 @@ program
 .option('-ph --phpmyadmin', "upgrade phpmyadmin")
 .option('-f --full', "Upgrade full php version")
 .option('-d --dir <value>', "Select your xampp installation directory")
+.parse(process.argv)
 
 const options = program.opts();
 
 let user_dir = options.dir ?? DEFAULT_DIR
-
-// if(options.php)
-// {
+console.log(process.argv);
+switch(process.platform)
+{
+    case 'win32': 
+        console.log('running on windows machine')
+    break;
+    default: 
+        console.error('not avaliable for your os =>', process.platform)
+        console.log('we are currently building one for it')
+        const _p = process.env.NODE_ENV || 'development'
+        if(_p != 'development') exit(1)
+        console.log(_p)
+    break;
+}
+if(options.php)
+{
     console.log('hello')
-    updatePHP(options.dir ?? './test/2/php')
-// }
+    updatePHP(options.dir ?? './test_zip/2/php/')
+    .then(d => {
+        if(d) console.log("php update successfull")
+    })
+}
 
 console.log('###################################### 100%')
 console.log('done')
